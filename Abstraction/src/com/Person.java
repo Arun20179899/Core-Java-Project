13e33354412fe0a9e15a9002c32b80f662451441
Arun Kumar A S <arun1998.ag12@gmail.com>
2024-12-27 10:10:44 +0530

    First Commit

---
+++ Abstraction/src/com/Person.java
@@ -0,0 +1,5 @@
+package com;
+
+public abstract class Person {
+	abstract void work();
+}
---
+++ Abstraction/src/com/Test.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.work();
+
+		System.out.println("-------------");
+
+		Person p = new Employee();
+		p.work();
+
+	}
+
+}
